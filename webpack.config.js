17531692d5f3f3d66d52e8e78d09ca9d57beb283
goodjwon <jwon76@naver.com>
2019-09-08 10:00:09 +0900

    webpack scss 관련 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
-const path = require('path');
-const webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
 
 module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
-        publicPath: "/dist"
+        filename: 'bundle.js',
+        publicPath: '/dist'
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.js$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
             }
         ]
     },
     plugins: [
+        extractPlugin
     ]
-}
+};
